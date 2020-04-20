e2dffbfa6e751178b1133e1106ad30991941d5fa
SaGonz <langSaGonz@gmail.com>
2020-04-20 19:49:55 +0200

    ROUTING TEST 4

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ conexion.connect(err => {
 })
 
 //Construir archivos de React desde Node 
-app.get('/*', (req, res) => {
+app.get('/app*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
